1b64169e20c3abb662b8cc96bccc56cede44b4a1
Joji Doi <joji.doi@gmail.com>
2016-08-25 18:14:54 -0500

    Fixed typo

--- src/main/java/com/djd/fun/tachchapter/demo002/Demo002.java
+++ src/main/java/com/djd/fun/tachchapter/demo002/Demo002.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 public class Demo002 {
 
   public static final void main(String [] params) {
-//  ArrayList<String>[] strings = new ArrayList<String[1]; // Compile error
+//  ArrayList<String>[] strings = new ArrayList<String>[1]; // Compile error
     ArrayList<String>[] strings = new ArrayList[1];
   }
 }
