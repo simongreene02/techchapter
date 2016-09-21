3eca80d8a00709180c5b82177a2ee6b8ff24c676
Ninja <ninja@renton.net>
2016-09-20 19:44:28 -0500

    demo 007: word counts

---
+++ src/main/java/com/djd/fun/tachchapter/demo007/RegExSplit.java
@@ -0,0 +1,45 @@
+package com.djd.fun.tachchapter.demo007;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.Maps;
+
+/**
+ * Count unique words
+ *
+ * @author JGD
+ * @since 9/20/16
+ */
+public class RegExSplit {
+  private final Map<String, AtomicInteger> wordCounts;
+
+  public RegExSplit() {
+    wordCounts = Maps.newHashMap();
+  }
+
+  public Map<String, AtomicInteger> countWords(String sentences) {
+    if (Strings.isNullOrEmpty(sentences)) {
+      return ImmutableMap.of();
+    }
+    String[] words = sentences.split("[\\.\\s',:]");
+    Stream.of(words)
+        .filter(word -> !word.isEmpty())
+        .map(String::toLowerCase)
+        .forEach(this::count);
+
+    return wordCounts;
+  }
+
+  private void count(String word) {
+    AtomicInteger count = wordCounts.get(word);
+    if (count == null) {
+      count = new AtomicInteger();
+      wordCounts.put(word, count);
+    }
+    count.incrementAndGet();
+  }
+}
---
+++ src/test/java/com/djd/fun/tachchapter/demo007/RegExSplitTest.java
@@ -0,0 +1,38 @@
+package com.djd.fun.tachchapter.demo007;
+
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import jdk.nashorn.internal.ir.annotations.Immutable;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * @author JGD
+ * @since 9/20/16
+ */
+public class RegExSplitTest {
+  private RegExSplit regExSplit;
+
+  @Before
+  public void setUp() {
+    regExSplit = new RegExSplit();
+  }
+
+  @Test
+  public void countWords() {
+    regExSplit.countWords("After beating the eggs, Dana read the next step:");
+    Map<String, AtomicInteger> counts = regExSplit.countWords("Add milk and eggs, then add flour and sugar.");
+    assertThat(counts.keySet()).containsExactlyElementsIn(
+        ImmutableSet.of("after", "beating", "the", "eggs", "dana", "read", "next", "step",
+            "add", "milk", "and", "then", "flour", "sugar"));
+    assertThat(counts.values().stream().filter(count -> count.get() == 1).count()).isEqualTo(10);
+    assertThat(counts.values().stream().filter(count -> count.get() == 2).count()).isEqualTo(4);
+  }
+}
